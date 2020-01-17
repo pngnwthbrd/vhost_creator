f3febb218be9be76db720b84ee46d6d082b266e0
pngnwthbrd <coding@pngnwthbrd.com>
2020-01-17 20:16:21 +0100

    add restart of httpd service

--- vhost_creator.py
+++ vhost_creator.py
@@ -11,23 +11,23 @@ def createVHOST(argv):
         except IndexError:
                 print('missing argument - plese enter a vhost name')
         else:
-                if vhost_name != "":
-                        with open('vhost_creator_conf.json', 'r') as cf:
-                                conf = json.load(cf)
-                                
-                        path = conf['server_path'] + vhost_name
-                        try:
-                                os.mkdir(path)
-                                uid = int(os.popen('id -u ' + conf['user']).read())
-                                gid = int(os.popen('id -g ' + conf['user']).read())
-                                os.chown(path, uid, gid)
-                        except OSError:
-                                print('Creation of the directory %s failed' % path)
-                        else:
-                                print('directory successfuly created.')
-                                
-                        try:
-                                vhost_conf = '''\n
+                with open('vhost_creator_conf.json', 'r') as cf:
+                        conf = json.load(cf)
+                        
+                path = conf['server_path'] + vhost_name
+                try:
+                        os.mkdir("%s" % path)
+                        uid = int(os.popen('id -u ' + conf['user']).read())
+                        gid = int(os.popen('id -g ' + conf['user']).read())
+                        os.chown(path, uid, gid)
+                except OSError:
+                        print('Creation of the directory %s failed' % path)
+                        sys.exit()
+                else:
+                        print('directory successfuly created.')
+                        
+                try:
+                        vhost_conf = '''\n
 <VirtualHost *:80>
     ServerAdmin webmaster@''' + vhost_name + '''.dom
     DocumentRoot "''' + conf['server_path'] + vhost_name + '''"
@@ -40,21 +40,25 @@ def createVHOST(argv):
     </Directory>
 </VirtualHost>
 '''
-                                copyfile('/etc/httpd/conf/extra/httpd-vhosts.conf', '/etc/httpd/conf/extra/httpd-vhosts.conf.bu')
-                                with open('/etc/httpd/conf/extra/httpd-vhosts.conf', 'a') as f:
-                                        f.write(vhost_conf + "\n\n")
-                                        
-                                copyfile('/etc/hosts', '/etc/hosts.bu')
-                                with open('/etc/hosts', 'a') as f:
-                                        f.write("\n127.0.0.1 " + vhost_name + ".dom")
+                        copyfile('/etc/httpd/conf/extra/httpd-vhosts.conf', '/etc/httpd/conf/extra/httpd-vhosts.conf.bu')
+                        with open('/etc/httpd/conf/extra/httpd-vhosts.conf', 'a') as f:
+                                f.write(vhost_conf + "\n\n")
                                 
-                        except OSError:
-                                print('Creation backup files failed')
-                        else:
-                                print('Creation backup files successful')
-                                
-                        print("Done!")
-                        return 1
+                        copyfile('/etc/hosts', '/etc/hosts.bu')
+                        with open('/etc/hosts', 'a') as f:
+                                f.write("\n127.0.0.1 " + vhost_name + ".dom")
+                        
+                except OSError:
+                        print('Creation backup files failed')
+                else:
+                        print('Creation backup files successful')
+                        
+                try:
+                       os.system('systemctl restart httpd.service')
+                       print('Httpd.service restart successful')
+                except SystemError:
+                        print('Restart httpd.service failed\nPlease restart manually')
+                print("Done!")
 
 if __name__ == "__main__":
         createVHOST(sys.argv)
